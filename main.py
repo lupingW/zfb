d254c8d789446f2b71a1dc90972eb4ecb219a9e4
lupingW <50098740+lupingW@users.noreply.github.com>
2024-12-06 10:19:09 +0800

    Update main.py

--- main.py
+++ main.py
@@ -80,8 +80,8 @@ def getBeijinTime():
         max_1 = 3500 * max_ratio
         min_1 = int(K * min_1)
         max_1 = int(K * max_1)
-        min_1 = math.min(min_1, 90000)
-        max_1 = math.min(max_1, 98000)
+        min_1 = min(min_1, 90000)
+        max_1 = min(max_1, 98000)
     else:
         print("获取北京时间失败")
         return
